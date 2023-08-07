9f9f950b57e21524057c4fd71be4b23b9a6b566d
Crowdbotics <team@crowdbotics.com>
2023-08-07 20:33:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dlees-ice-cream-tru-42711",
+  url: "https://dlees-ice-cream-tru-42711.botics.co",
+  api: "https://dlees-ice-cream-tru-42711.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
